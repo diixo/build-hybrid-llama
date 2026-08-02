0e5b851f32f94ac08d23a8bc5e6fb76ca5ee526e
diixo <anatolii.podoba@gmail.com>
2026-08-02 04:41:17 +0300

    test_hf.py updated

--- test_hf.py
+++ test_hf.py
@@ -1,8 +1,8 @@
 
 import json
-from auto_config import AutoConfigModel
 import torch
 from transformers import GPT2TokenizerFast
+from modeling_gptr import GPTRForCausalLM
 
 
 if __name__ == "__main__":
@@ -10,9 +10,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"using device: {device}")
 
-    tokenizer = GPT2TokenizerFast.from_pretrained(f"aitetic/gpt-r-0.3b", local_files_only=False)
-
-    model = AutoConfigModel.from_pretrained("aitetic/gpt-r-0.3b", map_location=device)
+    model = GPTRForCausalLM.from_pretrained("aitetic/gpt-r-0.3b", map_location=device)
 
     if model is None:
         raise SystemExit("Checkpoint 'aitetic/gpt-r-0.3b' was not found on Hugging Face Hub or in the local cache.")
@@ -24,10 +22,18 @@ if __name__ == "__main__":
     print(json.dumps(config_dict, indent=2, ensure_ascii=False, default=str))
 
     model = model.to(device)
-    
-    input_ids = tokenizer("Neural Network", return_tensors="pt").input_ids.to(device)
-    text = model.generate(input_ids, max_new_tokens=50, do_sample=False, eos_token_id=tokenizer.eos_token_id)
+    ############################################################################
 
-    gen_ids = text[0].detach().cpu().tolist()
+    tokenizer = GPT2TokenizerFast.from_pretrained(f"aitetic/gpt-r-0.3b", local_files_only=False)
 
-    print(f"Generated text: {tokenizer.decode(gen_ids, skip_special_tokens=True)}")
+    prompt = "The future of AI is"
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+    generated_ids = model.generate(
+        input_ids,
+        max_new_tokens=50,
+        do_sample=False, eos_token_id=tokenizer.eos_token_id)
+
+    output_ids = generated_ids[0].detach().cpu().tolist()
+
+    print(f"\nGenerated text: {tokenizer.decode(output_ids, skip_special_tokens=True)}")
+
